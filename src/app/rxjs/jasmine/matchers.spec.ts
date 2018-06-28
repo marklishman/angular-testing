55469a7a177ae99dc7f7d7b65a699bc3b2e10e37
marklishman <mark.lishman@gmail.com>
2018-06-28 10:39:13 +0100

    jasmine spies

--- src/app/rxjs/jasmine/matchers.spec.ts
+++ src/app/rxjs/jasmine/matchers.spec.ts
@@ -16,26 +16,23 @@
 describe('Jasmine Matchers', () => {
 
   it('actual values should match truthy and falsy', () => {
-
     expect(1).toBeTruthy();
     expect(0).toBeFalsy();
-
   });
 
   it('not should negate the condition', () => {
-
     expect( null).not.toBeTruthy();
-
   });
 
   it('toBe and toEqual should check for equality', () => {
-
     expect(1).toBe(1.0);
     expect(1).toEqual(1.0);
-
   });
 
   it('toBe and toEqual should behave differently for objects', () => {
+
+    // toBe uses ===
+    // toEqual uses deep equality comparison
 
     const obj = { a: 123 };
     expect(obj).toBe(obj);
@@ -48,35 +45,43 @@ describe('Jasmine Matchers', () => {
     const different = { a: 321 };
     expect(obj).not.toBe(different);
     expect(obj).not.toEqual(different);
-
   });
 
-  it('should', () => {
-
-    // expect(123.45).toBeCloseTo(123.44, 1);
-    // expect({}).toBeDefined();
-    // expect(3).toBeGreaterThan(2);
-    // expect(number).toBeLessThan(number);
-    // expect(number).toBeNaN();
-    // expect(mixed).toBeNull();
-    // expect(mixed).toBeUndefined();
-    // expect(array).toContain(member);
-    // expect(mixed).toEqual(mixed);
-
-    // expect(spy).toHaveBeenCalled();
-    // expect(spy).toHaveBeenCalledTimes(number);
-    // expect(spy).toHaveBeenCalledWith(...arguments);
-    // expect(mixed).toMatch(pattern);
-    // expect(fn).toThrow(string);
-    // expect(fn).toThrowError(string);
-    //
-    // jasmine.any(Constructor);
-    // jasmine.anything(mixed);
-    // jasmine.arrayContaining(mixed);
-    // jasmine.objectContaining(mixed);
-    // jasmine.stringMatching(pattern);
-
-    expect().nothing();
-
+  it('should be close to', () => {
+    expect(123.45).toBeCloseTo(123.44, 1);
+    expect(123.45).not.toBeCloseTo(123.44, 2);
   });
+
+  it('should compare with', () => {
+    expect(3).toBeGreaterThan(2);
+    expect(3).toBeGreaterThanOrEqual(3);
+    expect(2).toBeLessThan(3);
+    expect(2).toBeLessThanOrEqual(2);
+  });
+
+  it('should check for null and undefined', () => {
+    let obj;
+    expect(obj).toBeUndefined();
+
+    obj = {};
+    expect(obj).toBeDefined();
+
+    obj = null;
+    expect(obj).toBeNull();
+    expect(obj).toBeDefined();
+  });
+
+  it('should check for NaN', () => {
+    expect(parseInt('xyz', 10)).toBeNaN();
+  });
+
+  it('should array to contain member', () => {
+    expect([10, 20, 30]).toContain(20);
+  });
+
+  it('should match', () => {
+    expect('Beware the moon').toContain('moo');
+    expect('Beware the moon').toMatch('.*moo.');
+  });
+
 });
