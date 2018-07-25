21e36c07d97ed30f5d24c14bd68e7a141f7b1bb5
marklishman <mark.lishman@gmail.com>
2018-07-25 11:16:20 +0100

    Renamed dependency to user

--- src/app/jasmine/spies.spec.ts
+++ src/app/jasmine/spies.spec.ts
@@ -17,66 +17,66 @@ class User {
 
 describe('Spies', () => {
 
-  let dependency: User;
+  let user: User;
 
   beforeEach(() => {
-    dependency = new User();
+    user = new User();
   });
 
   it('should work without spies', () => {
-    expect(dependency.fullName()).toBe('Bob Jones');
+    expect(user.fullName()).toBe('Bob Jones');
   });
 
   describe('spyOn', () => {
     it('should replace a method on an existing object', () => {
-      spyOn<User>(dependency, 'fullName').and.returnValue('Joe Smith');
-      expect(dependency.fullName()).toBe('Joe Smith');
+      spyOn<User>(user, 'fullName').and.returnValue('Joe Smith');
+      expect(user.fullName()).toBe('Joe Smith');
     });
   });
 
   describe('createSpy', () => {
     it('should replace a method on a new object', () => {
-      dependency = {
+      user = {
         firstName: null,
         lastName: null,
         fullName: createSpy().and.returnValue('Pete Wilson'),
         userName: createSpy().and.returnValue('petewilson')
       };
-      expect(dependency.fullName()).toBe('Pete Wilson');
-      expect(dependency.userName()).toBe('petewilson');
+      expect(user.fullName()).toBe('Pete Wilson');
+      expect(user.userName()).toBe('petewilson');
     });
 
     it('should replace a method on an existing object', () => {
-      dependency = new User();
-      dependency.fullName = createSpy().and.returnValue('Sarah Brooks');
-      expect(dependency.fullName()).toBe('Sarah Brooks');
+      user = new User();
+      user.fullName = createSpy().and.returnValue('Sarah Brooks');
+      expect(user.fullName()).toBe('Sarah Brooks');
     });
   });
 
   describe('createSpyObj', () => {
     it('should replace multiple methods using method names in an array', () => {
-      dependency = createSpyObj<User>(
+      user = createSpyObj<User>(
         [
           'fullName',
           'userName'
         ]
       );
-      (<Spy>dependency.fullName).and.returnValue('Sue Robson');
+      (<Spy>user.fullName).and.returnValue('Sue Robson');
 
-      expect(dependency.fullName()).toBe('Sue Robson');
-      expect(dependency.userName()).toBe(undefined);
+      expect(user.fullName()).toBe('Sue Robson');
+      expect(user.userName()).toBe(undefined);
     });
 
     it('should replace multiple methods with values using method names and values in an object', () => {
-      dependency = createSpyObj<User>(
+      user = createSpyObj<User>(
         {
           fullName: 'Rita Watson',
           userName: 'ritawatson'
         }
       );
 
-      expect(dependency.fullName()).toBe('Rita Watson');
-      expect(dependency.userName()).toBe('ritawatson');
+      expect(user.fullName()).toBe('Rita Watson');
+      expect(user.userName()).toBe('ritawatson');
     });
   });
 
